21731ebefdc4bc45a895fdeeedffd1b0cbc7389a
JvMaiia <joaovmferreira@gmail.com>
2016-12-07 23:38:32 -0200

    First commit & add clients/servers

--- client_tcp.py
+++
@@ -1,7 +0,0 @@
-import socket
-target_host = input("Which your target host? ")
-target_port = int(input("Which your target port? "))
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((target_host, target_port))
-data = client.recv(1024)
-print(data.decode('ascii'))
